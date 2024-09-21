397fe9ece876aea195acd1b0b4a7b804f9fe871a
Eliehome <elie.medioniwiii@gmail.com>
2024-09-21 11:58:34 +0300

    File to compare_performances.py

--- tests/parallel_performance_evaluation/compare_performances.py
+++ tests/parallel_performance_evaluation/compare_performances.py
@@ -2,11 +2,11 @@
 This script is used to compare the performances of the parallel version of the code.
 """
 
+import os
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 from time import time
 
-from tests.radiative_surface.radiative_surface_manager_test import num_ref_rectangles
-
+from src.radiance_comp_vf import RadiativeSurfaceManager
 
 # Geometry creation
 def make_geo(num_rectangles:int):
@@ -32,40 +32,92 @@ def make_inputs(radiative_surface_manager,path_root_simulation_folder,
     )
 
 # Run the simulation
-def run_sim_with_outputs():
-    pass
+def run_sim_with_outputs(radiative_surface_manager,nb_rays:int,
+            num_workers:int,
+            worker_batch_size:int,
+            executor_type):
+    radiative_surface_manager.run_vf_computation_in_parallel(
+            nb_rays=nb_rays,
+            num_workers=num_workers,
+            worker_batch_size=worker_batch_size,
+            executor_type=ThreadPoolExecutor
+        )
 
-def run_sim_without_outputs():
-    pass
+def run_sim_without_outputs(radiative_surface_manager,nb_rays:int,
+            num_workers:int,
+            worker_batch_size:int,
+            executor_type):
+    radiative_surface_manager.run_vf_computation_in_parallel_without_output_files(
+        nb_rays=nb_rays,
+        num_workers=num_workers,
+        worker_batch_size=worker_batch_size,
+        executor_type=ProcessPoolExecutor
+    )
+
 
 def main():
-
     # Geometry creation
-    num_ref_rectangles=100
+    num_ref_rectangles = 200
+    radiative_surface_manager=make_geo(num_ref_rectangles)
 
-    make_geo()
-    make_inputs()
+    # Parallel parameters
+    num_workers = 16
+    worker_batch_size = 10
+
+    # VF parameters
+    nb_rays = 10000
+
+
+    # Make input files
+    file_dir = os.path.dirname(os.path.abspath(__file__))
+    path_root_simulation_folder = os.path.join(file_dir, "outputs")
+
+    num_receiver_per_file = 100
+    make_inputs(radiative_surface_manager,path_root_simulation_folder,
+            num_receiver_per_file)
 
     # Simulation with outputs and Threading
     start = time()
-    run_sim_with_outputs()
+    run_sim_with_outputs(radiative_surface_manager,
+            nb_rays=nb_rays,
+            num_workers=num_workers,
+            worker_batch_size=worker_batch_size,
+            executor_type=ProcessPoolExecutor
+        )
     dur_with_outputs_threading = time() - start
     print(f"Simulation with outputs and threading : {dur_with_outputs_threading}")
     # Simulation with outputs and multiprocessing
     start = time()
-    run_sim_with_outputs()
+    run_sim_with_outputs(radiative_surface_manager,
+            nb_rays=nb_rays,
+            num_workers=num_workers,
+            worker_batch_size=worker_batch_size,
+            executor_type=ProcessPoolExecutor
+        )
     dur_with_outputs_multiprocessing = time() - start
     print(f"Simulation with outputs and multiprocessing : {dur_with_outputs_multiprocessing}")
 
     # Simulation without outputs and Threading
     start = time()
-    run_sim_without_outputs()
+    run_sim_without_outputs(radiative_surface_manager,
+            nb_rays=nb_rays,
+            num_workers=num_workers,
+            worker_batch_size=worker_batch_size,
+            executor_type=ProcessPoolExecutor
+        )
     dur_without_outputs_threading = time() - start
     print(f"Simulation without outputs and threading : {dur_without_outputs_threading}")
     # Simulation without outputs and multiprocessing
     start = time()
-    run_sim_without_outputs()
+    run_sim_without_outputs(radiative_surface_manager,
+            nb_rays=nb_rays,
+            num_workers=num_workers,
+            worker_batch_size=worker_batch_size,
+            executor_type=ProcessPoolExecutor
+        )
     dur_without_outputs_multiprocessing = time() - start
     print(f"Simulation without outputs and multiprocessing : {dur_without_outputs_multiprocessing}")
 
+if __name__ == "__main__":
+    main()
 
